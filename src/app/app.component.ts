3d24dfa2e72b1a230c9bfeeb40b17e8d657da5c1
Bogdan <bogdan_mykhailov@icloud.com>
2022-05-05 23:23:48 +0300

    angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CurrencyapidataService} from "./currencyapidata.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'converter';
+  currentJson: any = [];
+
+  base = 'UAH';
+  currency2 = 'UAH'
+  result: string = '1'
+
+  changeBase(a: string) {
+    this.base = a;
+
+  }
+
+  convertTo(b: string) {
+    this.currency2 = b
+
+  }
+
+  constructor(private currency: CurrencyapidataService) {
+  }
+
+convert() {
+  this.currency.getCurrencyData(this.base).subscribe(data => {
+
+    this.currentJson = JSON.stringify(data);
+
+    this.currentJson = JSON.parse(this.currentJson)
+    console.log(this.currentJson)
+
+    if (this.currency2 === 'USD') {
+      this.result = this.currentJson.rates.USD
+    }
+
+    if (this.currency2 === 'UAH') {
+      this.result = this.currentJson.rates.UAH
+    }
+
+    if (this.currency2 === 'EUR') {
+      this.result = this.currentJson.rates.EUR
+    }
+  })
+
+}
 }
---
+++ src/app/currencyapidata.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyapidataService {
+
+  constructor(private http:HttpClient) { }
+
+  getCurrencyData(currency1: string) {
+    let url = `https://api.exchangerate.host/latest?base=` + currency1
+    return this.http.get(url);
+  }
+}
